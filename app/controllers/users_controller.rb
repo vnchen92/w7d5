0706fbecb7085a4d26b2d9137a9a568871b1bf96
Gabriel <gabegroenendaal@gmail.com>
2022-05-13 10:51:29 -0400

    Phase 1 complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
       def create 
             @user = User.new(user_params)
             if @user.save
+                  login!(@user)
                   redirect_to user_url(@user)
             else 
                   flash.now[:errors] = @user.errors.full_messages
